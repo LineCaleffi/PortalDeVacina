a644e3def319fb86015b906c08abe6e5f7ad30bd
Aline <alinecaleffi97@gmail.com>
2022-05-16 15:32:24 -0300

    css

--- projeto-teste/src/app/usuario/usuario-routing.module.ts
+++ projeto-teste/src/app/usuario/usuario-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormUsuarioComponent } from './form-usuario/form-usuario.component';
 import { UsuarioListarComponent } from './usuario-listar/usuario-listar.component';
 
 const routes: Routes = [
   {path: 'usuarios', component: UsuarioListarComponent},
+  {path: 'usuarios/criar', component: FormUsuarioComponent},
 
 ];
 
--- projeto-teste/src/app/usuario/usuario.module.ts
+++ projeto-teste/src/app/usuario/usuario.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioListarComponent } from './usuario-listar/usuario-listar.component';
 import { UsuarioRoutingModule } from './usuario-routing.module';
+import { FormUsuarioComponent } from './form-usuario/form-usuario.component';
 
 
 
 @NgModule({
   declarations: [
-    UsuarioListarComponent
+    UsuarioListarComponent,
+    FormUsuarioComponent
   ],
   imports: [
     CommonModule,
